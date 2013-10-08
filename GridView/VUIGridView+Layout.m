edb23945d4ae7f1a777cf56609a5be153bd90a1f
Harry Li <harryl.byread@gmail.com>
2013-10-08 22:51:06 +0800

    prevent double click on the same button causing problem

--- GridView/VUIGridView+Layout.m
+++ GridView/VUIGridView+Layout.m
@@ -30,6 +30,8 @@
 }
 
 - (void)_clickCell:(VUIGridCellView*)cell {
+    if (cell.index == self.selectedIndex || cell.index == _selectedIndex)
+        return;
     SEL csass = sel_registerName("cellSwitchAnimationStopped");
     if ([self.delegate respondsToSelector:csass])
         if (!self.delegate.cellSwitchAnimationStopped)
